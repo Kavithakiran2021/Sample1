f0a2e7d7b3c5c8e91994c9146f1653e494235b78
Kavithakiran2021 <kavithakiran1106@gmail.com>
2021-11-10 19:34:33 +0530

    trying

--- src/org/test/Class.java
+++ src/org/test/Class.java
@@ -2,10 +2,8 @@ package org.test;
 
 public class Class {
 	public static void location() {
-		System.out.println("location");
+	 	System.out.println("location");
 	}
-	public static void main(String[] args) {
-		location();	
-		System.out.println("hotel");
+
 	}
-}
+
