005165a1c28468fe38d253c4691cf2f8d688a8a1
Kavithakiran2021 <kavithakiran1106@gmail.com>
2021-11-10 10:41:07 +0530

    location

--- src/org/test/Class.java
+++ src/org/test/Class.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class Class {
-
+	public static void location() {
+		System.out.println("location");
+	}
+	public static void main(String[] args) {
+		location();	
+	}
 }
