71b7e99f72a1a27afab497edea026bb82ecfec02
Kavithakiran2021 <kavithakiran1106@gmail.com>
2021-12-15 12:47:58 +0530

    login functionality

--- src/org/test/Class.java
+++ src/org/test/Class.java
@@ -3,6 +3,12 @@ package org.test;
 public class Class {
 	public static void location() {
 		System.out.println("location");
+		
+	}
+	public void coll() {
+		// TODO Auto-generated method stub
+		System.out.println("getti practice");
+
 	}
 	public static void main(String[] args) {
 		location();	
